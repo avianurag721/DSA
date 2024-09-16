49d580452dcf77166265e53e959a101cb086bceb
avianurag721 <anurag.721721@gmail.com>
2024-09-16 08:16:23 +0530

    rotating array

--- arrays/arr.cpp
+++ arrays/arr.cpp
@@ -17,7 +17,7 @@ using namespace std;
 //     return largestElement;
 // }
 
-// second largest element 
+// second largest element
 int print2largest(vector<int> &arr)
 {
     int largest = arr[0];
@@ -43,39 +43,83 @@ int print2largest(vector<int> &arr)
     return secondLargest;
 }
 
-bool isSorted( vector <int> &arr){
+bool isSorted(vector<int> &arr)
+{
     bool sorted = false;
-    for (int i = 0; i < arr.size();i++){
-        if(arr[i]<arr[i+1]){
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] < arr[i + 1])
+        {
             sorted = true;
-        cout << sorted;
+            cout << sorted;
         }
     }
-    if(sorted){
+    if (sorted)
+    {
         return true;
     }
     else
         return false;
 }
 
-remve duplicates from non decreasing array
-int removeDuplicates( vector <int> &arr){
-    int k=0;
-    for (int i = 0; i < arr.size();i++){
-        if(arr[i]!=arr[k]){
+// remve duplicates from non decreasing array
+int removeDuplicates(vector<int> &arr)
+{
+    int k = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] != arr[k])
+        {
             arr[k + 1] = arr[i];
             k++;
         }
     }
     return k + 1;
 }
+void rotateOneLeft(vector<int> arr)
+{
+    int temp = arr[0];
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[arr.size() - 1] = temp;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    //  return 0;
+}
+
+void rotate(vector <int> &arr,int start,int end){
+    while(start<=end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void rotateByKDigit(vector <int> &arr,int k){
+    k = k % arr.size();
+    rotate(arr, 0, k - 1);
+    rotate(arr, k,arr.size()-1 );
+    rotate(arr, 0,arr.size()-1 );
+     for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+}
 
 int main()
 {
 
-    vector<int> arr = {1,2,5,3,4,1,4,5,6,1,1};
-    int value = removeDuplicates(arr);
-    cout << (value);
+    vector<int> arr = {1,2,3,4,5,6,7};
+    rotateByKDigit(arr,5);
+
+    // cout << (arr);
 
     return 0;
 }
